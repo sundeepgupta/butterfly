c30a89d0038f77d4b56a85f47e85fa190405e9e2
Sundeep Gupta <sundeep@sundeepgupta.ca>
2015-04-21 21:23:39 -0400

    Update .gitignore.

--- butterfly/AppDelegate.swift
+++ butterfly/AppDelegate.swift
@@ -1,15 +1,15 @@
 import UIKit
 import Parse
 
-
 @UIApplicationMain
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        Parse.setApplicationId("Ntm7aZEBUH4qL1ydhkU3Y5slXmeC97zPZkSquZp4", clientKey: "tFg56Wn0aW1hl7fsSzcAFv6gumxaiMCF8EAio9v8")
+    func application(application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        Parse.setApplicationId("Ntm7aZEBUH4qL1ydhkU3Y5slXmeC97zPZkSquZp4",
+            clientKey: "tFg56Wn0aW1hl7fsSzcAFv6gumxaiMCF8EAio9v8")
                 
         return true
     }
